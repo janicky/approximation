7987fb2875885ede806327e5aa546194b405e472
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-08 00:43:59 +0200

    Add more assertions to legendre polynomial test

--- src/test/java/LegendreTest.java
+++ src/test/java/LegendreTest.java
@@ -9,6 +9,7 @@ class LegendreTest {
         assertEquals(1, Legendre.polynomial(0, 2));
         assertEquals(2, Legendre.polynomial(1, 2));
         assertEquals(5.5, Legendre.polynomial(2,2));
+        assertEquals(321, Legendre.polynomial(4,3));
     }
 
 }
