34f13fe3205d951656ac225e4ee6f9e199844501
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-08 11:52:13 +0200

    Add GUI form & main

---
+++ src/main/java/Application.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Application extends JFrame {
+
+    private JPanel applicationContent;
+    private JTable table;
+
+    public Application(String title) throws HeadlessException {
+        super(title);
+        setContentPane(applicationContent);
+        setSize(700, 500);
+        setVisible(true);
+        setLocationRelativeTo(null);
+    }
+}
