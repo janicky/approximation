57b1286dc17edb642a5b21b0b5bcfd845f7bea7a
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-08 10:51:28 +0200

    Add approx method to Approximation

--- src/main/java/Approximation.java
+++ src/main/java/Approximation.java
@@ -4,4 +4,12 @@ public class Approximation {
         return Integral.simpson(i) / Math.pow(i + 0.5, -1);
     }
 
+    public static double approx(double x) {
+        double result = 0d;
+        for (int i = 0; i <= Configuration.n; i++) {
+            result += lambda(i) * Legendre.polynomial(i, x);
+        }
+        return result;
+    }
+
 }
