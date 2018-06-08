b337a2a114b1e004222c4c8eed91f318ca0b20a6
Patryk Janicki <kontakt.janicki@gmail.com>
2018-06-08 10:48:15 +0200

    Add comments to Configuration

--- src/main/java/Configuration.java
+++ src/main/java/Configuration.java
@@ -1,7 +1,10 @@
 public class Configuration {
-    public static double a = -1;
-    public static double b = 1;
+
+    public static double a = -1; // przedział
+    public static double b = 1; // przedział
+
     public static int m = 10; // liczba przedziałów całkowania
+    public static int n = 3; // stopień wielomianu
 
     public static double function(double x) {
         return Math.PI - 0.4 * x * x * x;
